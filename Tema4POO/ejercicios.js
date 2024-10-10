3e648dd324a47f2a4d75fee5195f7f3eaedb8f5e
CristinaGrande <cgrandegb@gmail.com>
2024-10-10 14:11:33 +0200

    Ejercicios T8 POO

--- Tema4POO/ejercicios.js
+++ Tema4POO/ejercicios.js
@@ -56,9 +56,78 @@ function calcularCuboNumero(numero) {
    function buscarPalabra(cadena) {
 
     palabraEnMayusculas = cadena.toLocaleLowerCase();
+    let palabraNueva;
 
     console.log(palabraEnMayusculas);
+
+    for(let i = 0; i < palabraEnMayusculas.length; i++) {
+        if(palabraEnMayusculas[i] % 2 == 0) {
+            palabraNueva.push(palabraEnMayusculas[i]);
+        }
+    }
+
+    console.log(palabraNueva);
 
    }
 
    buscarPalabra("PALABRA");
+
+   //Ejercicio 7, punto 1
+
+   function numeroAleatorio() {
+
+    let numeroRandom = Math.round(Math.random() * 10);
+
+    console.log(numeroRandom);
+
+    sacar3NumerosAleatorios();
+   }
+
+numeroAleatorio();
+
+function sacar3NumerosAleatorios() {
+  
+    let arrayNumerosAleatorios = [];
+  
+      let numero1 = Math.random() * 101;
+      let numero2 = Math.random() * 101;
+      let numero3 = Math.random() * 101;
+  
+      console.log(numero1);
+      console.log(numero2);
+      console.log(numero3);
+  
+      let numeroMayor = Math.max(numero1, numero2, numero3);
+  
+      console.log("El mayor número es " + numeroMayor);
+  
+      Math.round(numero1, numero2, numero3);
+  
+      console.log(numero1);
+      console.log(numero2);
+      console.log(numero3);
+    
+    let numero1Round = Math.round(numero1);
+    let numero2Round = Math.round(numero2);
+    let numero3Round = Math.round(numero3);
+    
+    arrayNumerosAleatorios.push(numero1Round, numero2Round, numero3Round);
+    
+    console.log(arrayNumerosAleatorios);
+
+   let opcion = alert("¿Desea redondear las notas hacia arriba? (S/N)");
+
+    if(opcion == true) {
+        Math.ceil(arrayNumerosAleatorios);
+    } else {
+        Math.floor(arrayNumerosAleatorios);
+    }
+    
+  }
+  
+  sacar3NumerosAleatorios();
+
+// Ejercicio 7, punto 2
+
+
+
